b61f04f420cf5785da6dacca475f165e969e6df1
Gustavosz124 <boltxmittens124@gmail.com>
2020-10-29 00:54:06 -0600

    Conectamos con la base de datos MongoDB usando mongoose

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,21 @@
 
 var express = require("express");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/primera_db");
+var userSchemaJSON = {
+    email: String,
+    password: String
+};
+var Schema = mongoose.Schema;
+var user_schema = new Schema(userSchemaJSON);
+var User = mongoose.model("User",user_schema);
 
 var app = express();
+app.use(express.static("src"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({"extended": true}));
 
 app.set("view engine", "pug");
 app.set("views","./views");
@@ -11,11 +25,18 @@ app.get("/",function (req,res) {
 });
 
 app.get("/login",function (req,res) {
-    res.render("form1");
+    User.find(function (err,doc) {
+        console.log(doc);
+        res.render("form1");
+    });
 })
 
-app.post("/",function (req,res) {
-    res.render("form1");
+app.post("/users",function (req,res) {
+    var user = new User({email: req.body.email, password: req.body.pass});
+
+    user.save(function () {
+        res.send("Tus datos han sido guardados");
+    })
 });
 
 app.listen(8080);
