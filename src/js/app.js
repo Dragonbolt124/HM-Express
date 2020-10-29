51e5a2a3b6032f965331e1cdd7fddd5dd6e5541d
Gustavosz124 <boltxmittens124@gmail.com>
2020-10-28 22:07:17 -0600

    Layout form con bootstraps

--- src/js/app.js
+++ src/js/app.js
@@ -6,9 +6,16 @@ var app = express();
 app.set("view engine", "pug");
 app.set("views","./views");
 
-
 app.get("/",function (req,res) {
     res.render("index",{nombre: "Gustavo"});
+});
+
+app.get("/login",function (req,res) {
+    res.render("form1");
+})
+
+app.post("/",function (req,res) {
+    res.render("form1");
 });
 
 app.listen(8080);
